c36ee8d3d3e338f0ae10831a93cf9d7cc11db274
Eduardo Marra <eduardomarra@gmail.com>
2022-06-01 17:17:29 -0300

    added scroll animation to sections

--- js/module/scrollReveal.js
+++ js/module/scrollReveal.js
@@ -1,5 +1,24 @@
 export default function ScrollAnimation(){
-    ScrollReveal({ reset: true });
-    ScrollReveal().reveal('.carrousel-categories', { delay: 500, duration: 1000 });
-    ScrollReveal().reveal('.carrousel-featured', { delay: 500, duration: 1000 });
+    ScrollReveal({ reset: false });
+
+    //SECTION CAROUSEL-CATEGORIES
+    ScrollReveal().reveal('section.carrousel-categories', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION CAROUSEL-FEATURED
+    ScrollReveal().reveal('section.carrousel-featured', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION BANNER
+    ScrollReveal().reveal('section.banner', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION CAROUSEL-DAILY
+    ScrollReveal().reveal('section.carrousel-daily', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION RATING
+    ScrollReveal().reveal('section.rating', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION SHOP-APP
+    ScrollReveal().reveal('section.shop-app', { delay: 500, duration: 1000, scale: 0});
+
+    //SECTION FOOTER
+    ScrollReveal().reveal('footer', { delay: 500, duration: 1000, scale: 0});
 }
